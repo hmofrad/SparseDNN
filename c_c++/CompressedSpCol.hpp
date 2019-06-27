c5500071cbc04790d093a836af9de2df45b2db36
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-06-26 23:52:38 -0400

    Update CompressedSpCol.hpp

--- c_c++/CompressedSpCol.hpp
+++ c_c++/CompressedSpCol.hpp
@@ -14,15 +14,16 @@
 template<typename Weight>
 struct CSC {
     public:
+        CSC() {nnz = 0; ncols = 0;};
         CSC(uint64_t nnz_,  uint32_t ncols_);
         ~CSC();
         void populate(std::vector<struct Triple<Weight>> triples);
         void walk();
         uint64_t nnz;
         uint32_t ncols;
-        void *IA; // Rows
-        void *JA; // Cols
-        void *A;  // Vals
+        uint32_t *IA; // Rows
+        uint32_t *JA; // Cols
+        Weight *A;  // Vals
 };
 
 template<typename Weight>
@@ -30,19 +31,19 @@ CSC<Weight>::CSC(uint64_t nnz_, uint32_t ncols_) {
     nnz = nnz_;
     ncols = ncols_;
     if(nnz and ncols) {
-        if((IA = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+        if((IA = (uint32_t *) mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
             fprintf(stderr, "Error mapping memory\n");
             exit(1);
         }
         memset(IA, 0, nnz * sizeof(uint32_t));
         
-        if((JA = mmap(nullptr, (ncols + 1) * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+        if((JA = (uint32_t *) mmap(nullptr, (ncols + 1) * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
             fprintf(stderr, "Error mapping memory\n");
             exit(1);
         }
         memset(JA, 0, (ncols + 1) * sizeof(uint32_t));
         
-        if((A = mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+        if((A = (Weight *) mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
             fprintf(stderr, "Error mapping memory\n");
             exit(1);
         }
@@ -68,14 +69,18 @@ CSC<Weight>::~CSC(){
             exit(1);
         }
     }
-    printf("deallocate???\n");
+    //printf("DEALLOCATE\n");
 }
 
 template<typename Weight>
 void CSC<Weight>::populate(std::vector<struct Triple<Weight>> triples) {
-    uint32_t *IA = (uint32_t*) this->IA;
-    uint32_t *JA = (uint32_t*) this->JA;
-    uint32_t *A  = (uint32_t*) this->A;
+    //uint32_t *IA = (uint32_t *) this->IA;
+    //uint32_t *JA = (uint32_t *) this->JA;
+    //Weight *A  = (Weight *) this->A;
+    
+    //uint32_t *IA = (uint32_t *) IA;
+    ///uint32_t *JA = (uint32_t *) JA;
+    //Weight *A  = (Weight *)      A;
     
     uint32_t i = 0;
     uint32_t j = 1;
@@ -85,7 +90,7 @@ void CSC<Weight>::populate(std::vector<struct Triple<Weight>> triples) {
             j++;
             JA[j] = JA[j - 1];
         }                  
-        A[i] = 1;
+        A[i] = triple.weight;
         JA[j]++;
         IA[i] = triple.row;
         i++;
@@ -98,14 +103,22 @@ void CSC<Weight>::populate(std::vector<struct Triple<Weight>> triples) {
 
 template<typename Weight>
 void CSC<Weight>::walk() {
-    uint32_t* A  = (uint32_t*) this->A;
-    uint32_t* IA = (uint32_t*) this->IA;
-    uint32_t* JA = (uint32_t*) this->JA;
+    //uint32_t *IA = (uint32_t*) this->IA;
+    //uint32_t *JA = (uint32_t*) this->JA;
+    //Weight   *A  = (Weight*)   this->A;
+    
+    //uint32_t *IA = (uint32_t*) IA;
+    //uint32_t *JA = (uint32_t*) JA;
+    //Weight   *A  = (Weight*)    A;
+    
     for(uint32_t j = 0; j < ncols; j++) {
         printf("j=%d\n", j);
         for(uint32_t i = JA[j]; i < JA[j + 1]; i++) {
-            printf("    i=%d, j=%d, value=%d\n", IA[i], j, A[i]);
+            IA[i];
+            A[i];
+            printf("    i=%d, j=%d, value=%f\n", IA[i], j, A[i]);
         }
+        
     }
 }
 
