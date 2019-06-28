740da8dcd892505aaf58f13bd59793c0788b96a0
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-06-28 14:59:52 -0400

    Update CompressedSpMat.hpp

--- c_c++/CompressedSpMat.hpp
+++ c_c++/CompressedSpMat.hpp
@@ -9,14 +9,8 @@
 #ifndef CompressedSpMat_HPP
 #define CompressedSpMat_HPP
 
-#include <sys/mman.h>
-#include <cstring> 
 #include "Allocator.hpp"
 #include "triple.hpp"
-
-
-
-
 
 template<typename Weight>
 struct CSC {
@@ -26,9 +20,9 @@ struct CSC {
         ~CSC();
         void populate(std::vector<struct Triple<Weight>> &triples);
         void walk();
-        uint64_t nnonzeros() { return(nnz); };
-        uint64_t nofrows() { return(nrows); };
-        uint64_t nofcols() { return(ncols); };
+        uint64_t numnonzeros() const { return(nnz); };
+        uint64_t numofrows()   const { return(nrows); };
+        uint64_t numofcols()   const { return(ncols); };
         uint32_t nrows;
         uint32_t ncols;
         uint64_t nnz;
@@ -49,102 +43,25 @@ CSC<Weight>::CSC(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_) {
     IA = nullptr;
     JA = nullptr;
     A  = nullptr;
-    
-    //printf("0.%p\n", IA);
     IA_blk = new Data_Block<uint32_t>(&IA, nnz, nnz * sizeof(uint32_t));
-    //printf("???\n");
-    //printf("??? %d\n", IA[0]);
-    
-    //printf("3.%p %p\n", IA, IA_blk->ptr);
-    //IA_blk->~Data_Block();
-    //IA = nullptr;
-    //delete IA_blk;
-    //IA = nullptr;
-    //struct Data_Block<uint32_t> IA_blsk;
-    //printf("%p %d\n", IA, IA[0]);
-    //printf("%p %p\n", IA, *(IA_blk->ptr));
-    
     JA_blk = new Data_Block<uint32_t>(&JA, (ncols + 1), (ncols + 1) * sizeof(uint32_t));
     A_blk  = new Data_Block<Weight>(&A,  nnz, nnz * sizeof(Weight));
-    
-    //exit(0);
-    //IA_blk.ptr = &IA;
-    //IA_blk.nitems = nnz;
-    //IA_blk.nbytes = nnz * sizeof(uint32_t);
-    //IA_blk.allocate();
-    
-    
-    /*
-    JA = nullptr;
-    JA_blk.ptr = &JA;
-    JA_blk.nitems = (ncols + 1);
-    JA_blk.nbytes = (ncols + 1) * sizeof(uint32_t);
-    
-    A = nullptr;
-    A_blk.ptr = &A;
-    A_blk.nitems = nnz;
-    A_blk.nbytes = nnz * sizeof(Weight);
-    */
-    //printf("%p %p\n", &IA, IA_blk.ptr);
-    //exit(0);
-    
-    /*
-    if(ncols and nnz) {
-        if((IA = (uint32_t *) mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(IA, 0, nnz * sizeof(uint32_t));
-        
-        if((JA = (uint32_t *) mmap(nullptr, (ncols + 1) * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(JA, 0, (ncols + 1) * sizeof(uint32_t));
-        
-        if((A = (Weight *) mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(A, 0, nnz * sizeof(Weight));
-    }
-    */
 }
 
 template<typename Weight>
 CSC<Weight>::~CSC(){
-    //printf("deleting\n");
     delete IA_blk;
     IA = nullptr;
     delete JA_blk;
     JA = nullptr;
     delete  A_blk;
     A  = nullptr;
-    
-    /*
-    if(ncols and nnz) {
-        if(munmap(A, nnz * sizeof(uint32_t)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-        
-        if(munmap(IA, nnz * sizeof(uint32_t)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-        
-        if(munmap(JA, (ncols + 1) * sizeof(uint32_t)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-    }
-    */
 }
 
 template<typename Weight>
 void CSC<Weight>::populate(std::vector<struct Triple<Weight>> &triples) {
     if(ncols and nnz) {
-        ColSort<double> f_col;
+        ColSort<Weight> f_col;
         std::sort(triples.begin(), triples.end(), f_col);
         
         uint32_t i = 0;
@@ -155,11 +72,12 @@ void CSC<Weight>::populate(std::vector<struct Triple<Weight>> &triples) {
                 j++;
                 JA[j] = JA[j - 1];
             }                  
-            A[i] = triple.weight;
             JA[j]++;
             IA[i] = triple.row;
+            A[i] = triple.weight;
             i++;
         }
+        
         while((j + 1) < ncols) {
             j++;
             JA[j] = JA[j - 1];
@@ -175,6 +93,128 @@ void CSC<Weight>::walk() {
             printf("    i=%d, j=%d, value=%f\n", IA[i], j, A[i]);
         }
         
+    }
+}
+
+template<typename Weight>
+struct CSR {
+    public:
+        CSR() { nrows = 0, ncols = 0; nnz = 0; IA = nullptr; JA = nullptr; A = nullptr; };
+        CSR(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_);
+        ~CSR();
+        void populate(std::vector<struct Triple<Weight>> &triples);
+        void walk();
+        uint64_t numnonzeros() const { return(nnz); };
+        uint64_t numofrows()   const { return(nrows); };
+        uint64_t numofcols()   const { return(ncols); };
+        uint32_t nrows;
+        uint32_t ncols;
+        uint64_t nnz;
+        uint32_t *IA; // Rows
+        uint32_t *JA; // Cols
+        Weight   *A;  // Vals
+        struct Data_Block<uint32_t> *IA_blk;
+        struct Data_Block<uint32_t> *JA_blk;
+        struct Data_Block<Weight>  *A_blk;
+        
+};
+
+template<typename Weight>
+CSR<Weight>::CSR(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_) {
+    nrows = nrows_;
+    ncols = ncols_;
+    nnz   = nnz_;
+    IA = nullptr;
+    JA = nullptr;
+    A  = nullptr;
+    IA_blk = new Data_Block<uint32_t>(&IA, (nrows + 1), (nrows + 1) * sizeof(uint32_t));
+    JA_blk = new Data_Block<uint32_t>(&JA, nnz, nnz * sizeof(uint32_t));
+    A_blk  = new Data_Block<Weight>(&A,  nnz, nnz * sizeof(Weight));
+}
+
+template<typename Weight>
+CSR<Weight>::~CSR(){
+    delete IA_blk;
+    IA = nullptr;
+    delete JA_blk;
+    JA = nullptr;
+    delete  A_blk;
+    A  = nullptr;
+}
+
+template<typename Weight>
+void CSR<Weight>::populate(std::vector<struct Triple<Weight>> &triples) {
+    if(nrows and nnz) {
+        RowSort<Weight> f_row;
+        std::sort(triples.begin(), triples.end(), f_row);
+        
+        
+        uint32_t i = 1;
+        uint32_t j = 0;
+        IA[0] = 0;
+        for(auto& triple : triples) {
+            //printf("%d %d %d %d\n", triple.row, triple.col,i ,j);
+            while((i - 1) != triple.row) {
+                i++;
+                IA[i] = IA[i - 1];
+            }                  
+            IA[i]++;
+            JA[j] = triple.col;
+            A[j] = triple.weight;
+            j++;
+         //   */
+        }
+        printf("xxxx\n");
+        while((i + 1) < nrows) {
+            i++;
+            IA[i] = IA[i - 1];
+        }
+    }
+    
+}
+
+template<typename Weight>
+void CSR<Weight>::walk() {
+    for(uint32_t i = 0; i < nrows; i++) {
+        printf("i=%d\n", i);
+        for(uint32_t j = IA[i]; j < IA[i + 1]; j++) {
+            printf("    i=%d, j=%d, value=%f\n", i, JA[j], A[j]);
+        }
+    }
+}
+
+
+enum Compression_Type{ 
+    csr_only,
+    csc_only,
+    dual
+};
+
+template<typename Weight>
+struct Sparse {
+    public: 
+        Sparse() {csc = nullptr; csr = nullptr;};
+        Sparse(Compression_Type type_);
+        ~Sparse() {};
+        enum Compression_Type type;
+        struct CSC<Weight> *csc;
+        struct CSC<Weight> *csr;
+};
+
+template<typename Weight>
+Sparse<Weight>::Sparse(Compression_Type type_) {
+    type = type_;
+    if(type == csr_only) {
+        printf("csr_only=%d\n", type);
+    }
+    else if(type == csc_only) {
+        printf("csc_only=%d\n", type);
+    }
+    else if(type == dual) {
+        printf("dual=%d\n", type);
+    }
+    else {
+        printf("Error=%d\n", type);
     }
 }
 
