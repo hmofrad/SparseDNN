f803f7ab8a208f9745368739962d78f0c5a6c729
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-06-30 12:01:49 -0400

    Update Allocator.hpp

--- c_c++/Allocator.hpp
+++ c_c++/Allocator.hpp
@@ -6,29 +6,36 @@
 
 #ifndef ALLOCATOR_HPP
 #define ALLOCATOR_HPP
- 
- 
+
 #include <sys/mman.h>
+ 
+#include <unistd.h>
 #include <cstring> 
 
 template<typename Data_Type>
 struct Data_Block {
     public:
         Data_Block() { ptr = nullptr; nitems = 0; nbytes = 0; }
-        Data_Block(Data_Type** ptr_, uint64_t nitems_, uint64_t nbytes_);
+        Data_Block(Data_Type** ptr_, uint64_t nitems_, uint64_t nbytes_, bool page_aligned_ = false);
         ~Data_Block();
         void allocate();
+        void reallocate(uint64_t nitems_);
         void deallocate();
         uint64_t nitems;
         uint64_t nbytes;
         Data_Type* ptr;
+        uint64_t PAGE_SIZE;
+        bool page_aligned;
 };
 
 template<typename Data_Type>
-Data_Block<Data_Type>::Data_Block(Data_Type** ptr_, uint64_t nitems_, uint64_t nbytes_) {
+Data_Block<Data_Type>::Data_Block(Data_Type** ptr_, uint64_t nitems_, uint64_t nbytes_, bool page_aligned_) {
     nitems = nitems_; 
     nbytes = nbytes_; 
     ptr = nullptr; 
+    page_aligned = page_aligned_;
+    PAGE_SIZE = sysconf(_SC_PAGESIZE);
+    //printf("PAGE_SIZE=%lu\n" , PAGE_SIZE);
     allocate();
     *ptr_ = ptr;
 }
@@ -41,6 +48,10 @@ Data_Block<Data_Type>::~Data_Block() {
 template<typename Data_Type>
 void Data_Block<Data_Type>::allocate() {
     if(nbytes) {
+        if(page_aligned) {
+            nbytes += (PAGE_SIZE - (nbytes % PAGE_SIZE));
+        }
+
         if((ptr = (Data_Type*) mmap(nullptr, nbytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {  
             fprintf(stderr, "Error: Cannot map memory\n");
             exit(1);
@@ -48,6 +59,34 @@ void Data_Block<Data_Type>::allocate() {
         memset(ptr, 0,  nbytes); 
     }
 }
+
+template<typename Data_Type>
+void Data_Block<Data_Type>::reallocate(uint64_t nbytes_) {
+    if(nbytes) {
+        if(page_aligned) {
+            
+            uint64_t new_nbytes = nbytes_;
+            new_nbytes += (PAGE_SIZE - (nbytes_ % PAGE_SIZE));
+            uint64_t old_nbytes = nbytes;
+            printf("nitems_=%lu %lu %lu %p \n", nbytes_, old_nbytes, new_nbytes, ptr);
+            Data_Type* ptr1;
+            if((ptr1 = (Data_Type*) mremap(ptr, old_nbytes, new_nbytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) { 
+                fprintf(stderr, "Error: Cannot map memory %d\n", ptr1 == MAP_FAILED );
+                exit(1);
+            }
+            //memset(ptr, 0,  nbytes); 
+            //void *p0 = mremap(p2, 4096, 4096, MREMAP_MAYMOVE | MREMAP_FIXED, p1);
+    //if ( p0 == MAP_FAILED ) {
+      //  perror("mremap: mremap failed");
+        //return EXIT_FAILURE;
+    //}
+            
+            
+            
+        }
+    }
+}
+
 
 template<typename Data_Type>
 void Data_Block<Data_Type>::deallocate() {
