3adccadedb9fc0a15d1b60db1e2f894d2e67f932
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-04 08:02:13 -0400

    Update CompressedSpMat.hpp

--- c_c++/CompressedSpMat.hpp
+++ c_c++/CompressedSpMat.hpp
@@ -1,7 +1,6 @@
 /*
  * CompressedSpMat.hpp: Compressed Sparse Matrix formats
  * Compressed Sparse Column (CSC)
- * Compressed Sparse Row (CSR)
  * (C) Mohammad Hasanzadeh Mofrad, 2019
  * (e) m.hasanzadeh.mofrad@pitt.edu
  */
@@ -13,120 +12,6 @@
 
 #include "Allocator.hpp"
 #include "Triple.hpp"
-
-/*
-template<typename Weight>
-struct CompressedSpMat {
-    public:
-        CompressedSpMat() { nrows = 0, ncols = 0; nnz = 0; IA = nullptr; JA = nullptr; A = nullptr; }
-        virtual ~CompressedSpMat() { }
-        virtual void populate(std::vector<struct Triple<Weight>> &triples) {}
-        virtual void walk() {}
-        uint64_t numnonzeros() const { printf("parent\n"); return(nnz); }
-        uint64_t numrows()   const { return(nrows); }
-        uint64_t numcols()   const { return(ncols); }
-        uint32_t nrows;
-        uint32_t ncols;
-        uint64_t nnz;
-        uint32_t *IA; // Rows
-        uint32_t *JA; // Cols
-        Weight   *A;  // Vals
-        struct Data_Block<uint32_t> *IA_blk;
-        struct Data_Block<uint32_t> *JA_blk;
-        struct Data_Block<Weight>   *A_blk;
-};
-
-
-
-
-template<typename Weight>
-struct CSR : public CompressedSpMat<Weight> {{
-    public:
-        CSR() { nrows = 0, ncols = 0; nnz = 0; IA = nullptr; JA = nullptr; A = nullptr; }
-        CSR(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_);
-        virtual ~CSR();
-        virtual void populate(std::vector<struct Triple<Weight>> &triples);
-        virtual void walk();
-        uint64_t numnonzeros() const { return(nnz); };
-        uint64_t numrows()   const { return(nrows); };
-        uint64_t numcols()   const { return(ncols); };
-        uint32_t nrows;
-        uint32_t ncols;
-        uint64_t nnz;
-        uint32_t *IA; // Rows
-        uint32_t *JA; // Cols
-        Weight   *A;  // Vals
-        struct Data_Block<uint32_t> *IA_blk;
-        struct Data_Block<uint32_t> *JA_blk;
-        struct Data_Block<Weight>  *A_blk;
-        
-};
-
-template<typename Weight>
-CSR<Weight>::CSR(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_) {
-    nrows = nrows_;
-    ncols = ncols_;
-    nnz   = nnz_;
-    IA = nullptr;
-    JA = nullptr;
-    A  = nullptr;
-    IA_blk = new Data_Block<uint32_t>(&IA, (nrows + 1), (nrows + 1) * sizeof(uint32_t));
-    JA_blk = new Data_Block<uint32_t>(&JA, nnz, nnz * sizeof(uint32_t));
-    A_blk  = new Data_Block<Weight>(&A,  nnz, nnz * sizeof(Weight));
-}
-
-template<typename Weight>
-CSR<Weight>::~CSR(){
-    delete IA_blk;
-    IA = nullptr;
-    delete JA_blk;
-    JA = nullptr;
-    delete  A_blk;
-    A  = nullptr;
-}
-
-template<typename Weight>
-void CSR<Weight>::populate(std::vector<struct Triple<Weight>> &triples) {
-    if(nrows and nnz) {
-        RowSort<Weight> f_row;
-        std::sort(triples.begin(), triples.end(), f_row);
-        
-        
-        uint32_t i = 1;
-        uint32_t j = 0;
-        IA[0] = 0;
-        for(auto& triple : triples) {
-            while((i - 1) != triple.row) {
-                i++;
-                IA[i] = IA[i - 1];
-            }                  
-            IA[i]++;
-            JA[j] = triple.col;
-            A[j] = triple.weight;
-            j++;
-        }
-        while((i + 1) < nrows) {
-            i++;
-            IA[i] = IA[i - 1];
-        }
-    }
-    
-}
-
-template<typename Weight>
-void CSR<Weight>::walk() {
-    for(uint32_t i = 0; i < nrows; i++) {
-        printf("i=%d\n", i);
-        for(uint32_t j = IA[i]; j < IA[i + 1]; j++) {
-            printf("    i=%d, j=%d, value=%f\n", i, JA[j], A[j]);
-        }
-    }
-}
-
-*/
-
-
-
 
 template<typename Weight>
 struct CSC {
@@ -451,7 +336,7 @@ void CSC<Weight>::walk() {
     }
 }
 
-
+/*
 template<typename Weight>
 struct CSR {
     public:
@@ -655,7 +540,7 @@ void CSR<Weight>::walk() {
         }
     }
 }
-
+*/
 
 enum Compression_Type{ 
     csr_only,
@@ -666,14 +551,16 @@ enum Compression_Type{
 template<typename Weight>
 struct CompressedSpMat {
     public: 
-        CompressedSpMat() {csc = nullptr; csr = nullptr;};
+        CompressedSpMat() {csc = nullptr;};
         CompressedSpMat(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_, std::vector<struct Triple<Weight>> &triples, Compression_Type type_, std::vector<uint32_t> *rowncols_ = nullptr);
         ~CompressedSpMat();
         enum Compression_Type type;
         struct CSC<Weight> *csc;
-        struct CSR<Weight> *csr;
+        //struct CSR<Weight> *csr;
         uint64_t nbytes;
 };
+
+
 
 template<typename Weight>
 CompressedSpMat<Weight>::CompressedSpMat(uint32_t nrows_, uint32_t ncols_, uint64_t nnz_, std::vector<struct Triple<Weight>> &triples, Compression_Type type_, std::vector<uint32_t> *rowncols_) {
@@ -720,11 +607,13 @@ CompressedSpMat<Weight>::CompressedSpMat(uint32_t nrows_, uint32_t ncols_, uint6
     }
 }
 
+
 template<typename Weight>
 CompressedSpMat<Weight>::~CompressedSpMat() {
     if(type == csc_only) {
         delete csc;
     }
+    /*
     else if(type == csr_only) {
         delete csr;
     }
@@ -732,6 +621,7 @@ CompressedSpMat<Weight>::~CompressedSpMat() {
         delete csc;
         delete csr;
     }
+    */
     else {
         fprintf(stderr, "Error: Cannot find requested compression %d\n", type);
         exit(1);
