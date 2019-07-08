20829fd0fd295d681bdddebd1ceaade3174fe1fd
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-07 23:25:35 -0400

    Update baseline.cpp

--- c_c++/baseline.cpp
+++ c_c++/baseline.cpp
@@ -24,7 +24,6 @@
 
 using WGT = double; 
 Compression_Type CT = Compression_Type::dcsc_fmt;
-int num = 1;
 int main(int argc, char **argv) {
     printf("INFO: Welcome to Sparse Deep Neural Network Serial Implementation\n");
     
@@ -76,7 +75,6 @@ int main(int argc, char **argv) {
     struct CompressedSpMat<WGT> featuresSpMat((nrowsFeatures + 1), (Nneurons + 1), featuresTriples.size(), featuresTriples, CT);
     featuresTriples.clear();
     featuresTriples.shrink_to_fit();
-
     
     uint32_t maxLayers = atoi(argv[4]);
     std::vector<uint32_t> maxLayersVector = {1, 120, 480, 1192};
@@ -120,7 +118,7 @@ int main(int argc, char **argv) {
     std::vector<struct DenseVec<WGT>*> biasesDenseVec;
 
     printf("INFO: Start reading %d layer files\n", maxLayers);
-    maxLayers = num;
+    //maxLayers = num;
     auto start = std::chrono::high_resolution_clock::now();
     for(uint32_t i = 0; i < maxLayers; i++) {  
         std::string layerFile = ((std::string) argv[6]) + "/neuron" + std::to_string(Nneurons) + "/n" + std::to_string(Nneurons) + "-l" + std::to_string(i+1) + ".tsv";
