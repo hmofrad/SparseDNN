bc3ef1208bdbfb760ec8f85b25f6592008429511
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-11 17:11:51 -0400

    Update Env.hpp

--- c_c++/Env.hpp
+++ c_c++/Env.hpp
@@ -13,28 +13,34 @@ class Env {
     public:
         Env();
         static int nthreads;
-        static std::vector<uint64_t> start;
-        static std::vector<uint64_t> end;
-        static std::vector<uint64_t> offset;
-        static std::vector<uint64_t> length;
-        
+        static std::vector<uint64_t> start_col;
+        static std::vector<uint64_t> end_col;
+        static std::vector<uint64_t> start_nnz;
+        static std::vector<uint64_t> end_nnz;
+        static std::vector<uint64_t> length_nnz;
+        static std::vector<uint64_t> offset_nnz;
+
         static void init();
         static int env_get_num_threads();
         static void env_set_offset();
 };
 int Env::nthreads = 0;
-std::vector<uint64_t> Env::start;
-std::vector<uint64_t> Env::end;
-std::vector<uint64_t> Env::offset;
-std::vector<uint64_t> Env::length;
+std::vector<uint64_t> Env::start_col;
+std::vector<uint64_t> Env::end_col;
+std::vector<uint64_t> Env::start_nnz;
+std::vector<uint64_t> Env::end_nnz;
+std::vector<uint64_t> Env::length_nnz;
+std::vector<uint64_t> Env::offset_nnz;
 
 
 void Env::init() {
     nthreads = env_get_num_threads();
-    start.resize(nthreads);
-    end.resize(nthreads);
-    offset.resize(nthreads);
-    length.resize(nthreads);
+    start_col.resize(nthreads);
+    end_col.resize(nthreads);
+    start_nnz.resize(nthreads);
+    end_nnz.resize(nthreads);
+    length_nnz.resize(nthreads);
+    offset_nnz.resize(nthreads);
 }
 
 int Env::env_get_num_threads() {
@@ -46,14 +52,21 @@ int Env::env_get_num_threads() {
     return(nthreads_);
 }    
 
-void Env::env_set_offset() {
-    offset[0] = 0;
+void Env::env_set_offset() {    
+    offset_nnz[0] = 0;
+    start_nnz[0] = 0;
+    end_nnz[0] = length_nnz[0];
     for(uint32_t i = 1; i < Env::nthreads; i++) {
-        offset[i] = (length[i-1] + offset[i-1]);
+        start_nnz[i] = end_nnz[i-1];
+        end_nnz[i] = start_nnz[i] + length_nnz[i];
+        offset_nnz[i] = start_nnz[i];
+        //offset_nzz[i] = (length_nnz[-1] + offset_nnz[i-1]);
+        
     }
-   // for(uint32_t i = 0; i < Env::nthreads; i++) {
-     //   printf("%lu %lu %lu %lu\n", start[i], end[i], length[i], offset[i]);
-    //}
+    for(uint32_t i = 0; i < Env::nthreads; i++) {
+        printf("%lu %lu %lu %lu %lu %lu\n", start_col[i], end_col[i], start_nnz[i], end_nnz[i], length_nnz[i], offset_nnz[i]);
+    }
+    //exit(0);
 }
 
 #endif 
