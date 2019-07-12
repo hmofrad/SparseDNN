e6e69ac974e65e45c3fe1c309124ea6592697ad4
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-12 00:21:36 -0400

    Update baseline.cpp

--- c_c++/baseline.cpp
+++ c_c++/baseline.cpp
@@ -125,7 +125,7 @@ int main(int argc, char **argv) {
     struct Triple<WGT> layerTriple;  
     std::vector<struct CompressedSpMat<WGT>*> layersSpMat;
     std::vector<struct DenseVec<WGT>*> biasesDenseVec;
-    //maxLayers = 4;
+    //maxLayers = 1;
     printf("INFO: Start reading %d layer files\n", maxLayers);
     auto start = std::chrono::high_resolution_clock::now();
     for(uint32_t i = 0; i < maxLayers; i++) {  
