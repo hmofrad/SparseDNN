091b006f2ca630adad50a865d6e9d392c5d106f9
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-12 00:17:33 -0400

    Update Env.hpp

--- c_c++/Env.hpp
+++ c_c++/Env.hpp
@@ -19,9 +19,11 @@ class Env {
         static std::vector<uint64_t> end_nnz;
         static std::vector<uint64_t> length_nnz;
         static std::vector<uint64_t> offset_nnz;
+        static std::vector<uint64_t> indices_nnz;
 
         static void init();
         static int env_get_num_threads();
+        static void env_unset_offset();
         static void env_set_offset();
 };
 int Env::nthreads = 0;
@@ -31,7 +33,7 @@ std::vector<uint64_t> Env::start_nnz;
 std::vector<uint64_t> Env::end_nnz;
 std::vector<uint64_t> Env::length_nnz;
 std::vector<uint64_t> Env::offset_nnz;
-
+std::vector<uint64_t> Env::indices_nnz;
 
 void Env::init() {
     nthreads = env_get_num_threads();
@@ -41,6 +43,7 @@ void Env::init() {
     end_nnz.resize(nthreads);
     length_nnz.resize(nthreads);
     offset_nnz.resize(nthreads);
+    indices_nnz.resize(nthreads);
 }
 
 int Env::env_get_num_threads() {
@@ -52,7 +55,25 @@ int Env::env_get_num_threads() {
     return(nthreads_);
 }    
 
+void Env::env_unset_offset() {    
+    for(uint32_t i = 0; i < Env::nthreads; i++) {   
+        start_col[i] = 0;
+        end_col[i] = 0;
+        start_nnz[i] = 0;
+        end_nnz[i] = 0;
+        offset_nnz[i] = 0;
+        indices_nnz[i] = 0;
+        length_nnz[i] = 0;
+    }
+}
+
 void Env::env_set_offset() {    
+    for(uint32_t i = 0; i < Env::nthreads; i++) {   
+        start_nnz[0] = 0;
+        end_nnz[0] = 0;
+        offset_nnz[i] = 0;
+        indices_nnz[i] = 0;
+    }
     offset_nnz[0] = 0;
     start_nnz[0] = 0;
     end_nnz[0] = length_nnz[0];
@@ -63,9 +84,11 @@ void Env::env_set_offset() {
         //offset_nzz[i] = (length_nnz[-1] + offset_nnz[i-1]);
         
     }
+    /*
     for(uint32_t i = 0; i < Env::nthreads; i++) {
         printf("%lu %lu %lu %lu %lu %lu\n", start_col[i], end_col[i], start_nnz[i], end_nnz[i], length_nnz[i], offset_nnz[i]);
     }
+    */
     //exit(0);
 }
 
