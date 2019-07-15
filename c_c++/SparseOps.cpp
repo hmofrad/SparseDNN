a8b93582b84d3f5ed54a04f8d248fead6d5c5c1d
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-15 16:03:10 -0400

    Update SparseOps.cpp

--- c_c++/SparseOps.cpp
+++ c_c++/SparseOps.cpp
@@ -11,8 +11,8 @@
 #include "Env.hpp"
 
 template<typename Weight>
-inline void SpMM_Sym(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC,
-                        struct CSC<Weight> *C_CSC, struct DenseVec<Weight> *s, int tid) { 
+inline void SpMM_Sym(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC, struct CSC<Weight> *C_CSC, 
+                     struct DenseVec<Weight> *s, int tid) { 
     uint32_t *A_JA = A_CSC->JA;
     uint32_t *A_IA = A_CSC->IA;      
     Weight   *A_A  = A_CSC->A;
@@ -25,62 +25,49 @@ inline void SpMM_Sym(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC,
     uint32_t B_nrows = B_CSC->nrows;  
     uint32_t B_ncols = B_CSC->ncols;
     
+    auto *s_A = s->A;
+    
     uint64_t nnzmax = 0;        
     if(A_ncols != B_nrows) {
         fprintf(stderr, "Error: SpMM dimensions do not agree A[%d %d] B[%d %d]\n", A_nrows, A_ncols, B_nrows, B_ncols);
         exit(1);
     }
-    //if(!tid)
-    Env::env_unset(tid);
-    //#pragma omp barrier
-    //#pragma omp parallel reduction(+:nnzmax)
-    //{
-        long nthreads = omp_get_num_threads();
-        //int tid = omp_get_thread_num();
-        uint32_t length = B_ncols;
-        uint32_t chunk = length/nthreads;
-        uint32_t start = chunk * tid;
-        uint32_t end = start + chunk;
-        end = (tid == nthreads - 1) ? length : end;
-        uint64_t nnzmax_local = 0;
 
-        auto *s_A = s->A;
-        //auto *s_A = s[tid]->A;
-        
-        for(uint32_t j = start; j < end; j++) {
-            for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
-                uint32_t l = B_IA[k];
-                for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
-                    s_A[A_IA[m]] = 1;
-                }
-            }
-            for(uint32_t i = 0; i < A_nrows; i++) {
-                if(s_A[i]) {
-                    nnzmax_local++;
-                    s_A[i] = 0;
-                }
+    Env::env_unset(tid);
+    int nthreads = omp_get_num_threads();
+    uint32_t length = B_ncols;
+    uint32_t chunk = length/nthreads;
+    uint32_t start = chunk * tid;
+    uint32_t end = start + chunk;
+    end = (tid == nthreads - 1) ? length : end;
+    uint64_t nnzmax_local = 0;
+    
+    for(uint32_t j = start; j < end; j++) {
+        for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+            uint32_t l = B_IA[k];
+            for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                s_A[A_IA[m]] = 1;
             }
         }
-
-        //nnzmax += nnzmax_local;
-        Env::start_col[tid] = start;
-        Env::end_col[tid] = end;
-        Env::length_nnz[tid] = nnzmax_local;
-    //}
+        for(uint32_t i = 0; i < A_nrows; i++) {
+            if(s_A[i]) {
+                nnzmax_local++;
+                s_A[i] = 0;
+            }
+        }
+    }
+    Env::start_col[tid] = start;
+    Env::end_col[tid] = end;
+    Env::length_nnz[tid] = nnzmax_local;
+        
     #pragma omp barrier
     if(!tid) {
-        //for(int i = 0; i < nthreads; i++) {
-          //  nnzmax += nnzmax_local
-            
-        //}
         nnzmax = Env::env_set();
         uint32_t nrows = A_CSC->nrows;
         uint32_t ncols = B_CSC->ncols;        
         C_CSC->initialize(nrows, ncols, nnzmax);
     }
     #pragma omp barrier
-//    printf("%lu/%lu\n", nnzmax, nnzmax_local);
-    //return(nnzmax);
 }
 
 template<typename Weight>
@@ -113,26 +100,22 @@ inline void SpMM(struct CSC<Weight> *A_CSC, struct CSC<Weight> *B_CSC, struct CS
         exit(1);
     }
 
-    //#pragma omp parallel
-    //{
-        //int tid = omp_get_thread_num();
-        uint32_t start = Env::start_col[tid];
-        uint32_t end = Env::end_col[tid];
-        auto *s_A = s->A;
-        //auto *s_A = s[tid]->A;
+    uint32_t start = Env::start_col[tid];
+    uint32_t end = Env::end_col[tid];
+    auto *s_A = s->A;
 
-        for(uint32_t j = start; j < end; j++) {
-            for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
-                uint32_t l = B_IA[k];
-                for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
-                    s_A[A_IA[m]] += B_A[k] * A_A[m];
-                }
+    for(uint32_t j = start; j < end; j++) {
+        for(uint32_t k = B_JA[j]; k < B_JA[j+1]; k++) {
+            uint32_t l = B_IA[k];
+            for(uint32_t m = A_JA[l]; m < A_JA[l+1]; m++) {
+                s_A[A_IA[m]] += B_A[k] * A_A[m];
             }
-            C_CSC->spapopulate_t(b, s, j, tid);
         }
-        #pragma omp barrier
-        C_CSC->postpopulate_t(tid);
-        A_CSC->repopulate(C_CSC, tid);
-    //}
+        C_CSC->spapopulate_t(b, s, j, tid);
+    }
+    #pragma omp barrier
+    C_CSC->postpopulate_t(tid);
+    A_CSC->repopulate(C_CSC, tid);
+    #pragma omp barrier
 }
 #endif
