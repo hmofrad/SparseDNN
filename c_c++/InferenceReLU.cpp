64fd0decfa700d29c94149bf555e0a5b830494cc
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-07-15 17:19:22 -0400

    Update InferenceReLU.cpp

--- c_c++/InferenceReLU.cpp
+++ c_c++/InferenceReLU.cpp
@@ -1,6 +1,6 @@
 /*
  * InferenceReLU.cpp: Inference Rectified Linear Unit (ReLU) implementation
- * (C) Mohammad Hasanzadeh Mofrad, 2019
+ * (c) Mohammad Hasanzadeh Mofrad, 2019
  * (e) m.hasanzadeh.mofrad@gmail.com
  */
  
